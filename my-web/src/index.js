e42a8a0ff8ad9d0df352c50f4f553c3b35452657
Glem2003 <glem920813@gmail.com>
2024-03-27 22:55:17 +0800

    Revise title

--- my-web/src/index.js
+++ my-web/src/index.js
@@ -5,6 +5,6 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>React 0327</h1>
+    <h1>Apple Store</h1>
   </React.StrictMode>
 );
