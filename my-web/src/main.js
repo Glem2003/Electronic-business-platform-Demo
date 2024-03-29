76d6df7d40391f5970bde02c5bb69f5cc678bb6f
Glem2003 <glem920813@gmail.com>
2024-03-29 23:25:32 +0800

    add content with "main & footer"

--- my-web/src/main.js
+++ my-web/src/main.js
@@ -2,29 +2,152 @@ import './main.sass';
 
 function Main() {
     return (
-        <Main>
+        <div className="main">
             {/* Start section1 */}
-            <div className="section1">
-                <div className="sec-title"></div>
-                <div className="sec-item"></div>
+            <div className="section section1">
+                <div className="sec-title">最新登場。<span style={{ color: 'grey' }}>現在就來看看有哪些新品推薦。</span></div>
+                <div className="sec-item">
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                </div>
             </div>
             {/*  End section1 */}
 
             {/* Start section2 */}
-            <div className="section2">
-                <div className="sec-title"></div>
-                <div className="sec-item"></div>
+            <div className="section section2">
+                <div className="sec-title">周邊配件 <span style={{ color: 'grey' }}>精彩配件，款款都是心愛裝置的必搭絕配。</span></div>
+                <div className="sec-item">
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                </div>
             </div>
             {/* End section2 */}
 
             {/* Start section3 */}
-            <div className="section3">
-                <div className="sec-title"></div>
-                <div className="sec-item"></div>
+            <div className="section section3">
+                <div className="sec-title">清亮動聽。<span style={{ color: 'grey' }}>層次豐富的高音質，款款都是好選擇。</span></div>
+                <div className="sec-item">
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                    <div className="item-col">
+                        <div className="col-wrapper"></div>
+                        <div className="col-info"></div>
+                    </div>
+                </div>
             </div>
             {/* End section3 */}
 
-        </Main>
+        </div>
     )
 }
 
