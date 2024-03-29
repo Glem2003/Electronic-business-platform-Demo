ec981d1f746b49fd1dd95c3d8f462b601ead8e3c
Glem2003 <164504533+Glem2003@users.noreply.github.com>
2024-03-29 09:31:52 +0000

    add "main" content

--- my-web/src/main.js
+++ my-web/src/main.js
@@ -1,9 +1,32 @@
 import './main.sass';
 
-function Main(){
-    return(
-        <h1>Main </h1>
+function Main() {
+    return (
+        <Main>
+            {/* Start section1 */}
+            <div className="section1">
+                <div className="sec-title"></div>
+                <div className="sec-item"></div>
+            </div>
+            {/*  End section1 */}
+
+            {/* Start section2 */}
+            <div className="section2">
+                <div className="sec-title"></div>
+                <div className="sec-item"></div>
+            </div>
+            {/* End section2 */}
+
+            {/* Start section3 */}
+            <div className="section3">
+                <div className="sec-title"></div>
+                <div className="sec-item"></div>
+            </div>
+            {/* End section3 */}
+
+        </Main>
     )
 }
 
 export default Main;
+//----------------------------------------------------
