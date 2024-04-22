3f2c706055c104c3f3bcd62511ffbb235bc28625
Glem2003 <glem920813@gmail.com>
2024-04-23 00:35:33 +0800

    ^^

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import { Route, Routes } from "react-router";
-
 import HomePages from './pages/HomePages';
 import StorePages from './pages/StorePages';
 
 const App = () => {
     return (
         <Routes>
-            <Route
-                index
-                path="/"
-                element={<HomePages/>}
-            />
-            <Route
-                index
-                path="/store"
-                element={<StorePages/>}
-            />
-        </Routes>
-
+			<Route
+				index
+				path="/"
+				element={<HomePages />}
+			/>
+			<Route
+				index
+				path="/store"
+				element={<StorePages />}
+			/>
+		</Routes>
     )
 }
 
