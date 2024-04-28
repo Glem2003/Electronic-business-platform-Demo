fd993052056b2a242a628214890c5b3b704d8349
Glem2003 <164504533+Glem2003@users.noreply.github.com>
2024-04-28 06:40:09 +0000

    add scrollbar.js

--- src/components/main/section.js
+++ src/components/main/section.js
@@ -1,20 +1,69 @@
+import React, { useState, useRef } from 'react';
 import SectionItem from "./section-item";
 
-function Section ({ title , subtitle, items }) {
+const Section = React.forwardRef(({ title, subtitle, items }, ref) => {
+
+    const ITEM_WIDTH = 500;
+
+    const [scrollPosition, setscrollPosition] = useState(0);
+    const [isHovered, setIsHovered] = useState(false);
+
+    const containerRef = useRef();
+
+    // Function to handle scrolling when the button is clicked
+    const handleScroll = (scrollAmount) => {
+        const newScrollPosition = scrollPosition + scrollAmount;
+        setscrollPosition(newScrollPosition);
+        containerRef.current.scrollLeft = newScrollPosition;
+    }
+
+    // Function to handle when the mouse is enter
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    }
+
     return (
-        <div className="section">
+        <div className="section" ref={ref}>
             <div className="sec-title">{title}<span style={{ color: 'grey' }}>{subtitle}</span></div>
-            <div className="sec-item">
-                {items.map((item,index) =>(
-                    <SectionItem 
-                        key={index} 
-                        {...item} 
-                        titleColor={item.titleColor} 
-                    />
-                ))}
+
+            <div className="scrollbar">
+                <div className="sec-item" ref={containerRef}>
+                    {items.map((item, index) => (
+                        <SectionItem
+                            key={index}
+                            {...item}
+                            titleColor={item.titleColor}
+                        />
+                    ))}
+                </div>
+            </div>
+
+            <div
+                className="action-btns"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            >
+                <button
+                    className="left-button"
+                    onClick={() => handleScroll(-ITEM_WIDTH)}
+                >
+                    L
+                </button>
+
+                <button
+                    className="right-button"
+                    onClick={() => handleScroll(ITEM_WIDTH)}
+                >
+                    R
+                </button>
+
             </div>
         </div>
     )
-}
+});
 
 export default Section;
