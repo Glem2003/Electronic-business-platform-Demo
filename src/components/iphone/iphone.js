635afb476815f04c925427d8f4657ae9268603c4
Glem2003 <glem920813@gmail.com>
2024-05-02 13:07:49 +0800

    change iphone-pages scroll-bar component

--- src/components/iphone/iphone.js
+++ src/components/iphone/iphone.js
@@ -5,7 +5,7 @@ import IphoneSecondSection from "./iphone-section2";
 
 const Iphone = () => {
     return (
-        <div className="Iphone">
+        <div className="iphone">
             <IphoneProductItems />
             <ProductInfoItem
                 textInfo="以iPhone 11 或後續機型升級換購 iPhone 15 或 iPhone 15 Pro。獲享 NT$4,500 至 NT$22,900 的折抵優惠。"
