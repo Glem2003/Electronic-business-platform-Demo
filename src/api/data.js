5a4e329f57056d8f40468b417364d6065466526a
Glem2003 <glem920813@gmail.com>
2024-05-06 22:57:38 +0800

    api response fail

--- src/api/data.js
+++
@@ -1,9 +0,0 @@
-const data = {
-    users: [
-      {id: 1, name: "Alice"},
-      {id: 2, name: "Bob"},
-      {id: 3, name: "Charlie"}
-    ]
-  };
-  
-  export default data;
--- src/index.js
+++ src/index.js
@@ -7,12 +7,29 @@ import './sass/style.sass';
 import './sass/default-layout.sass';
 
 
+const Index = () => {
+
+  React.useEffect(() => {
+    fetch("./api/data.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonData) => {
+        console.log(jsonData);
+      })
+      .catch((error) => {
+        console.log("Response error", error);
+      });
+  }, []);
+}
+
 //--------------------------------------------------------
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App/>
+      <Index />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
