b2d46be83497eece56e66d95bcb0ca49dd5faf14
Glem2003 <glem920813@gmail.com>
2024-05-11 21:40:27 +0800

    change navBar -first

--- src/components/dafault-layout/header/nav-bar.js
+++ src/components/dafault-layout/header/nav-bar.js
@@ -7,65 +7,37 @@ function NavBar() {
     const svgStyle = {width:"1.5em",height:"1.5em"}
 
     return (
-        <nav className="navBar">
-            <ul className="nav-item">
-                <NavBarLink
-                    to="/"
-                    children={
+        <nav>
+            <div className='content'>
+                <ul className='content-list'>
+
+                    <NavBarLink to="/">
                         <FaApple style={svgStyle}/>
-                    }
-                />
-                <NavBarLink
-                    to="/store"
-                    children="商店"
-                />
-                <NavBarLink
-                    to="/mac"
-                    children="Mac"
-                />
-                <NavBarLink
-                    to="/ipad"
-                    children="ipad"
-                />
-                <NavBarLink
-                    to="/iPhone"
-                    children="iPhone"
-                />
-                <NavBarLink
-                    to="/watch"
-                    children="Watch"
-                />
-                <NavBarLink
-                    to="/airPods"
-                    children="AirPods"
-                />
-                <NavBarLink
-                    to="/tv-and-family"
-                    children="TV 和家庭"
-                />
-                <NavBarLink
-                    to="/funny"
-                    children="娛樂"
-                />
-                <NavBarLink
-                    to="/component"
-                    children="配件"
-                />
-                <NavBarLink
-                    to="/other-serve"
-                    children="支援服務"
-                />
-                
-                <CiSearch  style={svgStyle}/>
+                    </NavBarLink>
 
-                <NavBarLink
-                    to="/cart"
-                    children={
-                        <FaShoppingCart  style={svgStyle}/>
-                    }
-                />
 
-            </ul>
+                    <NavBarLink>
+                        Store
+                    </NavBarLink>
+
+                    <NavBarLink>
+                        Mac
+                    </NavBarLink>
+
+                    <NavBarLink>
+                        iPhone
+                    </NavBarLink>
+
+
+                    <NavBarLink>
+                        <CiSearch style={svgStyle}/>
+                    </NavBarLink>
+                    <NavBarLink>
+                        <FaShoppingCart style={svgStyle}/>
+                    </NavBarLink>
+
+                </ul>
+            </div>
         </nav>
     )
 }
