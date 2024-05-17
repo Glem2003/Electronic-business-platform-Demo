ab5f1e23a2e5fea0eb13cf46a8e87651d8865d22
Glem2003 <164504533+Glem2003@users.noreply.github.com>
2024-05-17 13:16:18 +0000

    add navBar data

--- src/components/dafault-layout/header/nav-bar.js
+++ src/components/dafault-layout/header/nav-bar.js
@@ -1,10 +1,22 @@
 import React, { useState } from 'react';
 import NavBarLink from './nav-bar-link';
-import { navBarStoreList, navBarMacList } from '../../../content/content.js';
+
 import '../../../sass/component/nav-bar.sass';
 import { FaApple, FaShoppingCart } from 'react-icons/fa';
 import { CiSearch } from 'react-icons/ci';
 import { NavBarLargeList, NavBarSmallList } from '../../common/navbar-list.js';
+import {
+    navBarStoreList,
+    navBarMacList,
+    navBarIpadList,
+    navBarIphoneList,
+    navBarWatchList,
+    navBarAirpodsList,
+    navBarTvFamilyList,
+    navBarFunnyList,
+    navBarShopList,
+    navBarSupportList
+} from '../../../content/content.js';
 
 function NavBar() {
     const svgStyle = { width: "1.5em", height: "1.5em" };
@@ -14,6 +26,30 @@ function NavBar() {
 
     const selectedNavBarMacData = navBarMacList[0]
     const otherNavBarMacData = navBarMacList.slice(1)
+
+    const selectedNavBarIpadData = navBarIpadList[0]
+    const otherNavBarIpadData = navBarIpadList.slice(1)
+
+    const selectedNavBarIphoneData = navBarIphoneList[0]
+    const otherNavBarIphoneData = navBarIphoneList.slice(1)
+
+    const selectedNavBarWatchData = navBarWatchList[0]
+    const otherNavBarWatchData = navBarWatchList.slice(1)
+
+    const selectedNavBarAirpodsData = navBarAirpodsList[0]
+    const otherNavBarAirpodsData = navBarAirpodsList.slice(1)
+
+    const selectedNavBarTvFamilyData = navBarTvFamilyList[0]
+    const otherNavBarTvFamilyData = navBarTvFamilyList.slice(1)
+
+    const selectedNavBarFunnyData = navBarFunnyList[0]
+    const otherNavBarFunnyData = navBarFunnyList.slice(1)
+
+    const selectedNavBarShopData = navBarShopList[0]
+    const otherNavBarShopData = navBarShopList.slice(1)
+
+    const selectedNavBarSupportData = navBarSupportList[0]
+    const otherNavBarSupportData = navBarSupportList.slice(1)
 
     const [flyoutContent, setFlyoutContent] = useState({
         store: false,
@@ -117,10 +153,11 @@ function NavBar() {
             <div className={`flyout-content ${Object.values(flyoutContent).some((value) => value) ? 'show' : ''}`}>
                 <div onMouseLeave={handleMouseLeave} className={`content-item ${flyoutContent.store ? 'show' : ''}`} id="store">
                     <div className="content-group">
-                        <NavBarLargeList>
+                        <NavBarLargeList
                             title={selectedNavBarStoreData.title}
                             items={selectedNavBarStoreData.listItem}
-                        </NavBarLargeList>
+                        />
+
                         {otherNavBarStoreData.map((data, index) => (
                             <NavBarSmallList
                                 key={index}
@@ -132,10 +169,11 @@ function NavBar() {
                 </div>
                 <div onMouseLeave={handleMouseLeave} className={`content-item ${flyoutContent.mac ? 'show' : ''}`} id="mac">
                     <div className="content-group">
-                        <NavBarLargeList>
+                        <NavBarLargeList
                             title={selectedNavBarMacData.title}
                             items={selectedNavBarMacData.listItem}
-                            <hr/>
+                        >
+                            <br />
                             <h4>比較 Mac 模型</h4>
                             <h4>Mac 辦得到</h4>
                         </NavBarLargeList>
@@ -150,230 +188,140 @@ function NavBar() {
                 </div>
                 <div onMouseLeave={handleMouseLeave} className={`content-item ${flyoutContent.ipad ? 'show' : ''}`} id="ipad">
                     <div className="content-group">
-                        <ul>
-                            <h6>探索 iPad</h6>
-                            <h1>探索所有 iPad</h1>
-                            <h1>iPad Pro</h1>
-                            <h1>iPad Air</h1>
-                            <h1>iPad</h1>
-                            <h1>iPad mini</h1>
-                            <h1>Apple Pencil</h1>
-                            <h1>鍵盤</h1>
+                        <NavBarLargeList
+                            title={selectedNavBarIpadData.title}
+                            items={selectedNavBarIpadData.listItem}
+                        >
                             <br />
                             <h4>比較 iPad 機型</h4>
                             <h4>選 iPad 理由</h4>
-
-                        </ul>
-                        <ul>
-                            <h6>選購 iPad</h6>
-                            <h4>選購 iPad</h4>
-                            <h4>iPad 配件</h4>
-                            <h4>Apple Trade In 換購方案</h4>
-                            <h4>信用卡分期付款</h4>
-                        </ul>
-                        <ul>
-                            <h6>iPad 相關</h6>
-                            <h4>適用於 iPad 的 AppleCare+ 服務專案</h4>
-                            <h4>iPadOS 17</h4>
-                            <h4>Apple 出品的 App</h4>
-                            <h4>iCloud+</h4>
-                            <h4>教育</h4>
-                        </ul>
+                        </NavBarLargeList>
+                        {otherNavBarIpadData.map((data, index) => (
+                            <NavBarSmallList
+                                key={index}
+                                title={data.title}
+                                items={data.listItem}
+                            />
+                        ))}
                     </div>
                 </div>
                 <div onMouseLeave={handleMouseLeave} className={`content-item ${flyoutContent.iphone ? 'show' : ''}`} id="iphone">
                     <div className="content-group">
-                        <ul>
-                            <h6>探索 iPhone</h6>
-                            <h1>探索所有 iPhone</h1>
-                            <h1>iPhone 15 Pro</h1>
-                            <h1>iPhone 14</h1>
-                            <h1>iPhone 13</h1>
-                            <h1>iPhone SE</h1>
+                        <NavBarLargeList
+                            title={selectedNavBarIphoneData.title}
+                            items={selectedNavBarIphoneData.listItem}
+                        >
                             <br />
                             <h4>比較 iPhone 機型</h4>
                             <h4>從 Android 轉用</h4>
-
-                        </ul>
-                        <ul>
-                            <h6>選購 iPhone</h6>
-                            <h4>選購 iPhone</h4>
-                            <h4>iPhone 配件</h4>
-                            <h4>Apple Trade In 換購方案</h4>
-                            <h4>信用卡分期付款</h4>
-                        </ul>
-                        <ul>
-                            <h6>iPhone 相關</h6>
-                            <h4>iPhone 支援服務</h4>
-                            <h4>適用於 iPhone 的 AppleCare+ 服務專案</h4>
-                            <h4>iOS 17</h4>
-                            <h4>Apple 出品的 App</h4>
-                            <h4>iPhone 隱私權</h4>
-                            <h4>iCloud+</h4>
-                            <h4>Apple Pay</h4>
-                            <h4>Siri</h4>
-                        </ul>
+                        </NavBarLargeList>
+                        {otherNavBarIphoneData.map((data, index) => (
+                            <NavBarSmallList
+                                key={index}
+                                title={data.title}
+                                items={data.listItem}
+                            />
+                        ))}
                     </div>
                 </div>
                 <div onMouseLeave={handleMouseLeave} className={`content-item ${flyoutContent.watch ? 'show' : ''}`} id="watch">
                     <div className="content-group">
-                        <ul>
-                            <h6>探索 Watch</h6>
-                            <h1>探索所有的 Apple Watch</h1>
-                            <h1>Apple Watch Series 9</h1>
-                            <h1>Apple Watch Ultra 2</h1>
-                            <h1>Apple Watch SE</h1>
-                            <h1>Apple Watch Nike</h1>
-                            <h1>Apple Watch Hermès</h1>
+                        <NavBarLargeList
+                            title={selectedNavBarWatchData.title}
+                            items={selectedNavBarWatchData.listItem}
+                        >
                             <br />
                             <h4>比較 Watch 錶款</h4>
                             <h4>Apple Watch 的好處</h4>
-
-                        </ul>
-                        <ul>
-                            <h6>選購 Watch</h6>
-                            <h4>選購 Apple Watch</h4>
-                            <h4>Apple Watch 工坊</h4>
-                            <h4>Apple Watch 錶帶</h4>
-                            <h4>Apple Watch 配件</h4>
-                            <h4>Apple Trade In 換購方案</h4>
-                            <h4>信用卡分期付款</h4>
-                        </ul>
-                        <ul>
-                            <h6>Watch 相關</h6>
-                            <h4>Apple Watch 支援服務</h4>
-                            <h4>AppleCare+ 服務專案</h4>
-                            <h4>watchOS 10</h4>
-                            <h4>Apple 出品的 App</h4>
-                        </ul>
+                        </NavBarLargeList>
+                        {otherNavBarWatchData.map((data, index) => (
+                            <NavBarSmallList
+                                key={index}
+                                title={data.title}
+                                items={data.listItem}
+                            />
+                        ))}
                     </div>
                 </div>
                 <div onMouseLeave={handleMouseLeave} className={`content-item ${flyoutContent.airpods ? 'show' : ''}`} id="airpods">
                     <div className="content-group">
-                        <ul>
-                            <h6>探索 AirPods</h6>
-                            <h1>探索所有的 AirPods</h1>
-                            <h1>AirPods Pro 第2代</h1>
-                            <h1>AirPods 第2代</h1>
-                            <h1>AirPods 第3代</h1>
-                            <h1>AirPods Max</h1>
+                        <NavBarLargeList
+                            title={selectedNavBarAirpodsData.title}
+                            items={selectedNavBarAirpodsData.listItem}
+                        >
                             <br />
                             <h4>比較 AirPods 機型</h4>
-
-                        </ul>
-                        <ul>
-                            <h6>選購 AirPods</h6>
-                            <h4>選購 AirPods</h4>
-                            <h4>AirPods 配件</h4>
-                        </ul>
-                        <ul>
-                            <h6>AirPods 相關</h6>
-                            <h4>AirPods 支援服務</h4>
-                            <h4>適用於耳機的 AppleCare+ 服務專線</h4>
-                            <h4>Apple Music</h4>
-                        </ul>
+                        </NavBarLargeList>
+                        {otherNavBarAirpodsData.map((data, index) => (
+                            <NavBarSmallList
+                                key={index}
+                                title={data.title}
+                                items={data.listItem}
+                            />
+                        ))}
                     </div>
                 </div>
                 <div onMouseLeave={handleMouseLeave} className={`content-item ${flyoutContent.tvAndFamily ? 'show' : ''}`} id="tv-and-family">
                     <div className="content-group">
-                        <ul>
-                            <h6>探索 TV 和家庭</h6>
-                            <h1>探索 TV 和家庭</h1>
-                            <h1>Apple TV 4K</h1>
-                            <h1>HomePod</h1>
-                            <h1>HomePod mini</h1>
-                        </ul>
-                        <ul>
-                            <h6>選購 TV 和家庭</h6>
-                            <h4>選購 Apple TV 4K</h4>
-                            <h4>選購 HomePod</h4>
-                            <h4>選購 HomePod mini</h4>
-                            <h4>選購 Siri Remote</h4>
-                            <h4>TV 和家庭配件</h4>
-                        </ul>
-                        <ul>
-                            <h6>TV 和家庭相關</h6>
-                            <h4>Apple TV 支援服務</h4>
-                            <h4>HomePod 支援服務</h4>
-                            <h4>AppleCare+ 服務專案</h4>
-                            <h4>Apple TV App</h4>
-                            <h4>Apple TV+</h4>
-                            <h4>家庭 App</h4>
-                            <h4>Apple Music</h4>
-                            <h4>Siri</h4>
-                            <h4>AirPlay</h4>
-                        </ul>
+                        <NavBarLargeList
+                            title={selectedNavBarTvFamilyData.title}
+                            items={selectedNavBarTvFamilyData.listItem}
+                        />
+                        {otherNavBarTvFamilyData.map((data, index) => (
+                            <NavBarSmallList
+                                key={index}
+                                title={data.title}
+                                items={data.listItem}
+                            />
+                        ))}
                     </div>
                 </div>
                 <div onMouseLeave={handleMouseLeave} className={`content-item ${flyoutContent.funny ? 'show' : ''}`} id="funny">
                     <div className="content-group">
-                        <ul>
-                            <h6>探索娛樂</h6>
-                            <h1>探索娛樂</h1>
-                            <h1>Apple One</h1>
-                            <h1>Apple TV+</h1>
-                            <h1>Apple Music</h1>
-                            <h1>Apple Arcade</h1>
-                            <h1>Apple Podcast</h1>
-                            <h1>Apple Books</h1>
-                            <h1>Apple Store</h1>
-                        </ul>
-                        <ul>
-                            <h6>支援服務</h6>
-                            <h4>Apple TV+ 支援服務</h4>
-                            <h4>Apple Music 支援服務</h4>
-                        </ul>
+                        <NavBarLargeList
+                            title={selectedNavBarFunnyData.title}
+                            items={selectedNavBarFunnyData.listItem}
+                        />
+                        {otherNavBarFunnyData.map((data, index) => (
+                            <NavBarSmallList
+                                key={index}
+                                title={data.title}
+                                items={data.listItem}
+                            />
+                        ))}
                     </div>
                 </div>
                 <div onMouseLeave={handleMouseLeave} className={`content-item ${flyoutContent.shop ? 'show' : ''}`} id="shop">
                     <div className="content-group">
-                        <ul>
-                            <h6>選購配件</h6>
-                            <h1>選購各款配件</h1>
-                            <h1>Mac</h1>
-                            <h1>iPad</h1>
-                            <h1>iPhone</h1>
-                            <h1>Apple Watch</h1>
-                            <h1>AirPods</h1>
-                            <h1>TV 和家庭</h1>
-                        </ul>
-                        <ul>
-                            <h6>探索配件</h6>
-                            <h4>Apple 出品</h4>
-                            <h4>Beats by Dr.Dre</h4>
-                            <h4>AirTag</h4>
-                        </ul>
+                        <NavBarLargeList
+                            title={selectedNavBarShopData.title}
+                            items={selectedNavBarShopData.listItem}
+                        />
+                        {otherNavBarShopData.map((data, index) => (
+                            <NavBarSmallList
+                                key={index}
+                                title={data.title}
+                                items={data.listItem}
+                            />
+                        ))}
                     </div>
                 </div>
                 <div onMouseLeave={handleMouseLeave} className={`content-item ${flyoutContent.support ? 'show' : ''}`} id="support">
                     <div className="content-group">
-                        <ul>
-                            <h6>探索支援服務</h6>
-                            <h1>iPhone</h1>
-                            <h1>Mac</h1>
-                            <h1>iPad</h1>
-                            <h1>Watch</h1>
-                            <h1>AirPods</h1>
-                            <h1>Music</h1>
-                            <h1>TV</h1>
+                        <NavBarLargeList
+                            title={selectedNavBarSupportData.title}
+                            items={selectedNavBarSupportData.listItem}
+                        >
                             <br />
                             <h4>探索支援服務</h4>
-                        </ul>
-                        <ul>
-                            <h6>取得協助</h6>
-                            <h4>社群</h4>
-                            <h4>查看保固範圍</h4>
-                            <h4>維修</h4>
-                            <h4>聯絡我們</h4>
-                        </ul>
-                        <ul>
-                            <h6>實用主題</h6>
-                            <h4>取得 AppleCare+ 服務專案</h4>
-                            <h4>Apple ID 和密碼</h4>
-                            <h4>帳單和訂閱項目</h4>
-                            <h4>「尋找」App</h4>
-                            <h4>輔助使用</h4>
-                        </ul>
+                        </NavBarLargeList>
+                        {otherNavBarSupportData.map((data, index) => (
+                            <NavBarSmallList
+                                key={index}
+                                title={data.title}
+                                items={data.listItem}
+                            />
+                        ))}
                     </div>
                 </div>
                 <div onMouseLeave={handleMouseLeave} className={`content-item ${flyoutContent.search ? 'show' : ''}`} id="search">
--- src/content/content.js
+++ src/content/content.js
@@ -78,3 +78,252 @@ export const navBarMacList = ([
         ]
     }
 ])
+
+export const navBarIpadList = ([
+    {
+        title: "探索 iPad",
+        listItem: [
+            {subtitle:"探索所有 iPad"},
+            {subtitle:"iPad Pro"},
+            {subtitle:"iPad Air"},
+            {subtitle:"iPad"},
+            {subtitle:"iPad mini"},
+            {subtitle:"Apple Pencil"},
+            {subtitle:"鍵盤"},
+        ]
+    },
+    {
+        title: "選購 iPad",
+        listItem: [
+            {subtitle:"選購 iPad"},
+            {subtitle:"iPad 配件"},
+            {subtitle:"Apple Trade In 換購方案"},
+            {subtitle:"信用卡分期付款"}
+        ]
+    },
+    {
+        title: "iPad 相關",
+        listItem: [
+            {subtitle:"適用於 iPad 的 AppleCare+ 服務專案"},
+            {subtitle:"iPadOS 17"},
+            {subtitle:"Apple 出品的 App"},
+            {subtitle:"iCloud+"},
+            {subtitle:"教育"}
+        ]
+    }
+])
+
+export const navBarIphoneList =([
+    {
+        title: "探索 iPhone",
+        listItem: [
+            {subtitle:"探索所有 iPhone"},
+            {subtitle:"iPhone 15 Pro"},
+            {subtitle:"iPhone 14"},
+            {subtitle:"iPhone 13"},
+            {subtitle:"iPhone SE"},
+        ]
+    },
+    {
+        title: "選購 iPhone",
+        listItem: [
+            {subtitle:"選購 iPhone"},
+            {subtitle:"iPhone 配件"},
+            {subtitle:"Apple Trade In 換購方案"},
+            {subtitle:"信用卡分期付款"},
+        ]
+    },
+    {
+        title: "iPhone 相關",
+        listItem: [
+            {subtitle:"iPhone 支援服務"},
+            {subtitle:"適用於 iPhone 的 AppleCare+ 服務專案"},
+            {subtitle:"iOS 17"},
+            {subtitle:"Apple 出品的 App"},
+            {subtitle:"iPhone 隱私權"},
+            {subtitle:"iCloud+"},
+            {subtitle:"Apple Pay"},
+            {subtitle:"Siri"},
+        ]
+    }
+])
+
+export const navBarWatchList =([
+    {
+        title: "探索 Watch",
+        listItem: [
+            {subtitle:"探索所有的 Apple Watch"},
+            {subtitle:"Apple Watch Series 9"},
+            {subtitle:"Apple Watch Ultra 2"},
+            {subtitle:"Apple Watch SE"},
+            {subtitle:"Apple Watch Nike"},
+            {subtitle:"Apple Watch Hermès"}
+        ]
+    },
+    {
+        title: "選購 Watch",
+        listItem: [
+            {subtitle:"選購 Apple Watch"},
+            {subtitle:"Apple Watch 工坊"},
+            {subtitle:"Apple Watch 錶帶"},
+            {subtitle:"Apple Watch 配件"},
+            {subtitle:"Apple Trade In 換購方案"},
+            {subtitle:"信用卡分期付款"},
+        ]
+    },
+    {
+        title: "Watch 相關",
+        listItem: [
+            {subtitle:"Apple Watch 支援服務"},
+            {subtitle:"AppleCare+ 服務專案"},
+            {subtitle:"watchOS 10"},
+            {subtitle:"Apple 出品的 App"}
+        ]
+    }
+    
+])
+
+export const navBarAirpodsList =([
+    {
+        title: "探索 AirPods",
+        listItem: [
+            {subtitle:"探索所有的 AirPods"},
+            {subtitle:"AirPods Pro 第2代"},
+            {subtitle:"AirPods 第2代"},
+            {subtitle:"AirPods 第3代"},
+            {subtitle:"AirPods Max"}
+        ]
+    },
+    {
+        title: "選購 AirPods",
+        listItem: [
+            {subtitle:"選購 AirPods"},
+            {subtitle:"AirPods 配件"}
+        ]
+    },
+    {
+        title: "AirPods 相關",
+        listItem: [
+            {subtitle:"AirPods 支援服務"},
+            {subtitle:"適用於耳機的 AppleCare+ 服務專線"},
+            {subtitle:"Apple Music"}
+        ]
+    }
+])
+
+export const navBarTvFamilyList =([
+    {
+        title: "探索 TV 和家庭",
+        listItem: [
+            {subtitle:"探索 TV 和家庭"},
+            {subtitle:"探索 TV 和家庭"},
+            {subtitle:"Apple TV 4K"},
+            {subtitle:"HomePod"},
+            {subtitle:"HomePod mini"}
+        ]
+    },
+    {
+        title: "選購 TV 和家庭",
+        listItem: [
+            {subtitle:"選購 Apple TV 4K"},
+            {subtitle:"選購 HomePod"},
+            {subtitle:"選購 HomePod mini"},
+            {subtitle:"選購 Siri Remote"},
+            {subtitle:"TV 和家庭配件"}
+        ]
+    },
+    {
+        title: "TV 和家庭相關",
+        listItem: [
+            {subtitle:"Apple TV 支援服務"},
+            {subtitle:"HomePod 支援服務"},
+            {subtitle:"AppleCare+ 服務專案"},
+            {subtitle:"Apple TV App"},
+            {subtitle:"Apple TV+"},
+            {subtitle:"家庭 App"},
+            {subtitle:"Apple Music"},
+            {subtitle:"Siri"},
+            {subtitle:"AirPlay"}
+        ]
+    }
+])
+
+export const navBarFunnyList =([
+    {
+        title: "探索娛樂",
+        listItem: [
+            {subtitle:"探索娛樂"},
+            {subtitle:"Apple One"},
+            {subtitle:"Apple TV+"},
+            {subtitle:"Apple Music"},
+            {subtitle:"Apple Arcade"},
+            {subtitle:"Apple Podcast"},
+            {subtitle:"Apple Books"},
+            {subtitle:"Apple Store"}
+        ]
+    },
+    {
+        title: "支援服務",
+        listItem: [
+            {subtitle:"Apple TV+ 支援服務"},
+            {subtitle:"Apple Music 支援服務"}
+        ]
+    }
+])
+
+export const navBarShopList =([
+    {
+        title: "選購配件",
+        listItem: [
+            {subtitle:"選購各款配件"},
+            {subtitle:"Mac"},
+            {subtitle:"iPad"},
+            {subtitle:"iPhone"},
+            {subtitle:"Apple Watch"},
+            {subtitle:"AirPods"},
+            {subtitle:"TV 和家庭"}
+        ]
+    },
+    {
+        title: "探索配件",
+        listItem: [
+            {subtitle:"Apple 出品"},
+            {subtitle:"Beats by Dr.Dre"},
+            {subtitle:"AirTag"}
+        ]
+    }
+])
+
+export const navBarSupportList =([
+    {
+        title: "探索支援服務",
+        listItem: [
+            {subtitle:"iPhone"},
+            {subtitle:"Mac"},
+            {subtitle:"iPad"},
+            {subtitle:"Watch"},
+            {subtitle:"AirPods"},
+            {subtitle:"Music"},
+            {subtitle:"TV"},
+        ]
+    },
+    {
+        title: "取得協助",
+        listItem: [
+            {subtitle:"社群"},
+            {subtitle:"查看保固範圍"},
+            {subtitle:"維修"},
+            {subtitle:"聯絡我們"}
+        ]
+    },
+    {
+        title: "實用主題",
+        listItem: [
+            {subtitle:"取得 AppleCare+ 服務專案"},
+            {subtitle:"Apple ID 和密碼"},
+            {subtitle:"帳單和訂閱項目"},
+            {subtitle:"「尋找」App"},
+            {subtitle:"輔助使用"}
+        ]
+    }
+])
