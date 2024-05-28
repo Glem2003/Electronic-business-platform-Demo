30ed67aacd8079be84b0e1be4d9388e1d2f6a434
Glem2003 <164504533+Glem2003@users.noreply.github.com>
2024-05-28 12:49:14 +0000

    update tv-home component

--- src/components/Tv-home/tvHome.js
+++ src/components/Tv-home/tvHome.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { tvHomeProductData } from "../../Api";
-//import { tvfamilyContent } from '../../Content/content.js';
+import { tvfamilyContent } from '../../Content/content.js';
 
 //import component
 import ProductItem from "../Common/productItem";
+import SectionTitle from '../Common/sectionTitle.js';
+import { ProductCardWrapper, CardInfo } from '../Common/productCard.js';
+import ButtonItem from '../Common/buttonItem.js';
 
 const TvHome = () => {
 
@@ -23,7 +26,7 @@ const TvHome = () => {
 
     return (
         <div className="tv-home">
-            
+
             <div className="Product-items">
                 {data && data.map((item) => {
                     return (
@@ -39,7 +42,39 @@ const TvHome = () => {
             </div>
 
             <div className="section section1">
-                <h1>05/28 beginner</h1>
+                {tvfamilyContent && tvfamilyContent.map((item) => {
+                    return item.header && item.header.map((text, index) => {
+                        return (
+                            <SectionTitle
+                                key={index}
+                                title={text.title}
+                                subtitle={text.text}
+                            />
+                        )
+                    })
+                })}
+            </div>
+
+            <div className="section section2">
+                {tvfamilyContent && tvfamilyContent.map((item) => {
+                    return item.banner_item && item.banner_item.map((info, index) => {
+                        return (
+                            <ProductCardWrapper
+                                key={index}
+                                src={info.src}
+                            >
+                                <CardInfo
+                                    title={info.title}
+                                    subtitle={info.subtitle}
+                                    text={info.price}
+                                />
+                                <ButtonItem 
+                                    props={info.link}
+                                />
+                            </ProductCardWrapper>
+                        )
+                    })
+                })}
             </div>
 
         </div>
