bd9b70ce059a85d73415e843452f25c0131956dd
Glem <glem920813@gmail.com>
2024-06-18 19:05:20 +0800

    Updates

---
+++ static/js/238.bfe023d8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_web=self.webpackChunkmy_web||[]).push([[238],{47:(s,e,c)=>{c.r(e),c.d(e,{default:()=>i});var n=c(9919),a=c(579);const r=()=>(0,a.jsx)("div",{className:"cart",children:(0,a.jsx)("div",{className:"section section1",children:(0,a.jsx)("h1",{children:"Coming Soon"})})});var t=c(7542);const i=()=>{const s=t.K8.map((s=>s.cart));return(0,a.jsx)(n.A,{logoTitle:s,children:(0,a.jsx)(r,{})})}}}]);
+//# sourceMappingURL=238.bfe023d8.chunk.js.map
